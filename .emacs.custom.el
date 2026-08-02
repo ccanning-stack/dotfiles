88a82d44f0a4718bc9c6c1535bdaacba38e26d56
ccanning-stack <147844781+ccanning-stack@users.noreply.github.com>
2026-08-02 18:26:32 +0100

    Add Emacs configuration pt 2

---
+++ .emacs.custom.el
@@ -0,0 +1,8 @@
+(custom-set-variables
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
+ '(display-line-numbers-type 'relative)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(gruber-darker-theme)))
+(custom-set-faces)
